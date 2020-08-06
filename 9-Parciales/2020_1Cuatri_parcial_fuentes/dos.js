6b94fd75c0fe4f03c610e9b67b53a5c03560a7e8
ANRecalde <you@example.com>
2020-08-05 22:35:00 -0300

    Cambios 05/08

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,115 @@
 function mostrar()
 {
-  alert("dos");
+  var material;
+  var cantidad;
+  var precio;
+  var importeBruto;
+  var importeNeto;
+  var descuento;
+  var inicio;
+  var cantidadBolsas;
+  var precioCemento;
+  var precioCal;
+  var precioArena;
+  var mayorBolsas;
+  var mayorMaterial;
+  var materialMasCaro;
+  var mayorPrecio;
+
+  cantidadBolsas=0;
+  mayorBolsas=0;
+  materialMasCaro=0;
+
+
+  inicio=confirm("Desea iniciar la compra?");
+
+  while(inicio==true)
+  {
+  	material=prompt("Ingrese el producto");
+  	while(material!="arena" && material!="cal" && material!="cemento")
+  	{
+  		material=prompt("Reingrese el material");
+  	}
+  	cantidad=prompt("Ingrese la cantidad de bolsas");
+  	cantidad=parseInt(cantidad);
+  	precio=prompt("Ingrese el precio por bolsa");
+  	precio=parseInt(precio);
+  	while(precio<1)
+  	{
+  		precio=prompt("Reingrese el precio por bolsa");
+  	}
+
+  	cantidadBolsas=cantidadBolsas+cantidad;
+
+  	
+  	switch(material)
+  	{
+  		case "cal":
+  		{
+  			precioCal=cantidad*precio;
+  		}
+
+  		case "cemento":
+  		{
+  			precioCemento=cantidad*precio;
+  		}
+
+  		case "arena":
+  		{
+  			precioArena=cantidad*precio;
+  		}
+  	}
+
+  	importeBruto=precioArena+precioCemento+precioCal;
+
+  	if(cantidadBolsas>15 && cantidadBolsas<30)
+  	{
+  		descuento=0.15;
+  	}
+  	if(cantidadBolsas>30)
+  	{
+  		descuento=0.25;
+  	}
+
+  	importeNeto=importeBruto-(importeBruto*descuento);
+
+  	if(mayorBolsas==0)
+  	{
+  		mayorBolsas=cantidad;
+  		mayorMaterial=material;
+  	}
+  	else
+  	{
+  		if(cantidad>mayorBolsas)
+  		{
+  			mayorBolsas=cantidad;
+  			mayorMaterial=material;
+  		}
+
+  	}
+
+  	if(mayorPrecio==0)
+  	{
+  		mayorPrecio=precio;
+  		materialMasCaro=material;
+  	}
+  	else
+  	{
+  		if(precio>mayorPrecio)
+  		{
+  			mayorPrecio=precio;
+  			materialMasCaro=material;
+  		}
+
+  	}
+
+  	inicio=confirm("Desea ingresar otro producto?");	
+  }	
+
+  alert("El importe bruto es $"+ importeBruto);
+  alert("El importe neto es $ "+ importeNeto);
+  alert("El material con mayor cantidad de bolsas es " + mayorMaterial);
+  alert("El material más caro es "+ materialMasCaro);
+ 
 }
+//"" switch  while
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/tres.js
@@ -1,4 +1,87 @@
 function mostrar()
 {
-	alert("tres");
+	var nombre;
+	var edad;
+	var sexo;
+	var estadoCivil;
+	var temperatura;
+	var mayorTemperatura;
+	var nombreMayorTemperatura;
+	var cantidadViudos;
+	var cantidadHombresNoCasados;
+	var terceraEdadFiebre;
+	var promedioEdadHombresSolteros;
+	var edadHombresSolteros;
+	var cantidadHombresSolteros;
+	var inicio;
+
+	cantidadHombresSolteros=0;
+	edadHombresSolteros=0;
+	mayorTemperatura=0;
+	cantidadViudos=0;
+	cantidadHombresNoCasados=0;
+	terceraEdadFiebre=0;
+	
+	inicio=confirm("Desea ingresar un pasajero?");
+	while(inicio==true)	
+	{
+		nombre=prompt("Ingrese el nombre");
+		edad=prompt("Ingrese la edad");
+		edad=parseInt(edad);
+		sexo=prompt("Ingrese el sexo(f o m)");
+		while(sexo!="f" && sexo!="m")
+		{
+			sexo=prompt("Reingrese el sexo(f o m)");
+		}	
+		estadoCivil=prompt("Ingrese el estado civil");
+		while(estadoCivil!="soltero" && estadoCivil!="casado" && estadoCivil!="viudo")
+		{
+			estadoCivil=prompt("Reingrese el estado civil");
+		}	
+		temperatura=prompt("Ingrese la temperatura corporal");
+		
+		if(mayorTemperatura==0)
+		{
+			mayorTemperatura=temperatura;
+			nombreMayorTemperatura=nombre;
+		}
+		else
+		{
+			if(temperatura>mayorTemperatura)
+			{
+				mayorTemperatura=temperatura;
+				nombreMayorTemperatura=nombre;
+			}	
+		}
+
+		if(edad>17 && estadoCivil=="viudo")
+		{
+			cantidadViudos=cantidadViudos+1;
+		}
+
+		if(sexo=="m" && estadoCivil=="soltero" ||  estadoCivil=="viudo")
+		{
+			cantidadHombresNoCasados=cantidadHombresNoCasados+1;
+		}
+
+		if(edad>60 && temperatura>38)
+		{
+			terceraEdadFiebre=terceraEdadFiebre+1;
+		}
+
+		if(sexo=="m" && estadoCivil=="soltero")
+		{
+			edadHombresSolteros=edadHombresSolteros+edad;
+			cantidadHombresSolteros=cantidadHombresSolteros+1;
+		}	
+
+		inicio=confirm("Desea ingresar otro pasajero?");
+	}
+	promedioEdadHombresSolteros=(edadHombresSolteros/cantidadHombresSolteros);
+	alert("La persona con mayor temperatura es " + nombreMayorTemperatura)
+	alert("La cantidad de viudos es " + cantidadViudos);
+	alert("La cantidad de hombres que no están casados es " + cantidadHombresNoCasados);
+	alert("La cantidad de personas de la tercera edad y con fiebre son " + terceraEdadFiebre);
+	alert("El promedio de edad de los hombres solteros es " + promedioEdadHombresSolteros + "años");
 }
+//""  switch  while
